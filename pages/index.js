ca029ca2d222bc29ca09a57228dab4766e75b326
ronaldojds <ronaldojs.dev@gmail.com>
2025-11-06 00:10:19 +0000

    Modifica mensagem inicial do arquivo `index.js`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-  return <h1>Teste!</h1>;
+  return (
+    <>
+      <h1>Em construção!!!</h1>
+      <h1>Futuro site de notícias sobre jogos GACHA \o/</h1>
+    </>
+  );
 }
 
 export default Home;
